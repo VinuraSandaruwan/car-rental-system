82f2fa9ecf1196157965df5360c1145520c2dc61
VINURA_SANDARUWAN <vinurasandaruwan@gmail.com>
2025-12-14 12:40:55 +0530

    Update VehicleReturnController.java

--- return-service/src/main/java/com/example/return_service/controller/VehicleReturnController.java
+++ return-service/src/main/java/com/example/return_service/controller/VehicleReturnController.java
@@ -2,15 +2,18 @@ package com.example.return_service.controller;
 
 import com.example.return_service.data.VehicleReturn;
 import com.example.return_service.service.VehicleReturnService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
-@RequestMapping("/api/returns")
+@RequestMapping("/returns")
 public class VehicleReturnController {
 
+   // @Autowired
     private final VehicleReturnService service;
 
     public VehicleReturnController(VehicleReturnService service) {
